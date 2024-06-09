b6125847dcb8bb979c4c478e21b11361b301245d
MyeongJunKim <myeongjun222@naver.com>
2024-06-09 17:28:01 +0900

    code 분리

--- WebFrontEnd/practice/src/main.ts
+++ WebFrontEnd/practice/src/main.ts
@@ -1,37 +1,8 @@
-import FPS from './fps';
-import { draw } from './draw';
-import { worker } from './workerConnector';
-
+import { html, render } from './offscreenCanvas';
 import './style.css';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
-app.innerHTML = `
-  <div style="display: flex; flex-direction: column; row-gap: 12px; padding: 16px; box-sizing: border-box;">
-    <button class="btn">block main thread</button>
-    <canvas class="canvas" width="1200" height="200"></canvas>
-    <canvas class="offscreen-canvas" width="1200" height="200"></canvas>
-  </div>
-`;
-const canvas: HTMLCanvasElement = document.querySelector('.canvas')!;
-const offCanvas: HTMLCanvasElement =
-  document.querySelector('.offscreen-canvas')!;
-canvas.style.border = '1px solid black';
-offCanvas.style.border = '1px solid black';
+app.innerHTML = html;
 
-const mainThreadBlockBtn: HTMLButtonElement = document.querySelector('.btn')!;
-
-const fps = new FPS();
-const ctx = canvas.getContext('2d')!;
-fps.execute(draw(ctx), 60);
-
-const offscreen = offCanvas.transferControlToOffscreen();
-worker.postMessage({ canvas: offscreen }, [offscreen]);
-
-// 3초간 main thread를 blocking
-const blockThread = () => {
-  const start = Date.now();
-  while (Date.now() - start < 3000) {}
-};
-
-mainThreadBlockBtn.onclick = blockThread;
+render();
